8177f814650de8d30de29eea192db9df1f9f5abd
Frederik Bussmann <frederik@bussmann.io>
2023-06-27 18:23:11 +0200

    Updated settings + Updated compilation script + Updated document content

--- dev-ops/compile.sh
+++ dev-ops/compile.sh
@@ -19,28 +19,29 @@ cp -r ${INPUTDIR}/* ${OUTPUTDIR}/.
 
 # Compile document
 SECONDS=0
+ERROR=false
 pdflatex -recorder -jobname=mydocument --interaction=batchmode -draftmode -shell-escape ${ENTRYFILE}
-(cd ${OUTPUTDIR} && makeglossaries ${ENTRYFILE})
+makeglossaries ${ENTRYFILE}
 biber --quiet --input-directory=${OUTPUTDIR} --output-directory=${OUTPUTDIR} ${ENTRYFILE}
 pdflatex --interaction=batchmode -shell-escape ${ENTRYFILE}
-
-# Copy generated log file to build folder
-cp ${OUTPUTDIR}/${ENTRYFILE}.log ${OUTPUTPDFDIR}
 
 # Move PDF to build folder if generated successfully
 if [ -f "${OUTPUTDIR}/${ENTRYFILE}.pdf" ]; then
   mv ${OUTPUTDIR}/${ENTRYFILE}.pdf ${OUTPUTPDFDIR}
 
   LOGOUTPUT=$(printf "Compilation finished, elapsed time: %s seconds\n" ${SECONDS})
-
-  # Output log to stdout and logfile
-  echo "${LOGOUTPUT}" && echo "${LOGOUTPUT}" >> ${OUTPUTDIR}/${ENTRYFILE}.log
 else
+  ERROR=true
   LOGOUTPUT=$(printf "Compilation finished with errors, elapsed time: %s seconds\n" ${SECONDS})
-
-  # Output log to stdout and logfile
-  echo "${LOGOUTPUT}" && echo "${LOGOUTPUT}" >> ${OUTPUTDIR}/${ENTRYFILE}.log
-
-  # Return error code
-  return 1;
 fi
+
+# Output log to stdout and logfile
+echo "${LOGOUTPUT}" && echo "${LOGOUTPUT}" >> ${OUTPUTDIR}/${ENTRYFILE}.log
+
+# Copy generated log file to build folder
+cp ${OUTPUTDIR}/${ENTRYFILE}.log ${OUTPUTPDFDIR}
+
+# Return exit code 1 on error
+case $ERROR in
+  (true)    exit 1;;
+esac
